0c2dbfad56dda0895bc533a3826167a46f943a4a
Rodrigo Kochenburger <divoxx@gmail.com>
2010-03-02 15:46:44 -0800

    Attempt to fix test without success

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,30 +1,28 @@
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 
 require 'test/unit'
-require File.expand_path(File.join(File.dirname(__FILE__), '../../../../config/environment.rb'))
 require 'rubygems'
+require 'active_support'
+require 'active_record'
 require 'active_record/fixtures'
 
 config = YAML::load(IO.read( File.join(File.dirname(__FILE__),'database.yml')))
 
-# cleanup logs and databases between test runs
-#FileUtils.rm File.join(File.dirname(__FILE__), "debug.log"), :force => true
-FileUtils.rm File.join(RAILS_ROOT, config['sqlite3'][:dbfile]), :force => true
-
 ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), "debug.log"))
-ActiveRecord::Base.establish_connection(config[ENV['DB'] || 'sqlite3'])
+ActiveRecord::Base.establish_connection(config)
+
+FileUtils.rm_f config[:database]
 
 load(File.join(File.dirname(__FILE__), "schema.rb"))
 
-Test::Unit::TestCase.fixture_path = File.dirname(__FILE__) + "/fixtures/"
-$LOAD_PATH.unshift(Test::Unit::TestCase.fixture_path)
+ActiveSupport::TestCase.fixture_path = File.dirname(__FILE__) + "/fixtures/"
 
 class Test::Unit::TestCase #:nodoc:
   def create_fixtures(*table_names)
     if block_given?
-      Fixtures.create_fixtures(Test::Unit::TestCase.fixture_path, table_names) { yield }
+      Fixtures.create_fixtures(ActiveSupport::TestCase.fixture_path, table_names) { yield }
     else
-      Fixtures.create_fixtures(Test::Unit::TestCase.fixture_path, table_names)
+      Fixtures.create_fixtures(ActiveSupport::TestCase.fixture_path, table_names)
     end
   end
 
@@ -35,5 +33,4 @@ class Test::Unit::TestCase #:nodoc:
   self.use_instantiated_fixtures  = false
   
   # Instantiated fixtures are slow, but give you @david where you otherwise would need people(:david)
-  
 end
